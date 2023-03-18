34d7d0a60285bc5176342e3639af1b85fa2d84b1
Quinntyne Brown <quinntynebrown@gmail.com>
2023-03-18 08:36:59 -0400

    add client libs

--- src/WebApps/Commitments.App/projects/note/core/jest.config.js
+++ src/WebApps/Commitments.App/projects/note/core/jest.config.js
@@ -1,5 +1,8 @@
 // Copyright (c) Quinntyne Brown. All Rights Reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-export * from './constants';
+module.exports = {
+    preset: 'jest-preset-angular',
+    globalSetup: 'jest-preset-angular/global-setup'
+};
 
--- src/WebApps/Commitments.App/projects/note/core/src/public-api.ts
+++ src/WebApps/Commitments.App/projects/note/core/src/public-api.ts
@@ -1,5 +1,9 @@
 // Copyright (c) Quinntyne Brown. All Rights Reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-export * from './constants';
+/*
+ * Public API Surface of core
+ */
 
+
+export * from './lib';
